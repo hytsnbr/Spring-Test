457bc5e7e6b7ae6b0b2814bcec6de28a8d6509e7
hytsnbr10519 <haya524mwam@gmail.com>
2023-04-13 23:03:18 +0900

    [update] いろいろ改良

--- app/steam-tool/src/main/java/com/hytsnbr/steam_tool/dao/SteamDao.java
+++ app/steam-tool/src/main/java/com/hytsnbr/steam_tool/dao/SteamDao.java
@@ -9,19 +9,19 @@ public final class SteamDao {
     
     private static final String URL = "https://api.steampowered.com/ISteamWebAPIUtil/GetSupportedAPIList/v1/?key={key}";
     
-    private static final String STEAM_API_KEY = "4B4CA2713423B757D13ADAFE82C94D9D";
-    
     private static final RestTemplate restTemplate;
     
     static {
         restTemplate = new RestTemplate();
     }
     
-    private SteamDao() {}
+    private SteamDao() {
+        throw new IllegalStateException("Utility class");
+    }
     
-    public static GetSupportedApiListResponse getSupportedApiList() {
+    public static GetSupportedApiListResponse getSupportedApiList(String apiKey) {
         return restTemplate.getForObject(
-            UriComponentsBuilder.fromHttpUrl(URL).build(STEAM_API_KEY),
+            UriComponentsBuilder.fromHttpUrl(URL).build(apiKey),
             GetSupportedApiListResponse.class
         );
     }
